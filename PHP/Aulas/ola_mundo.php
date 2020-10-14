822ba68236011010fd258190bab8200d65433be5
LuizMiguelSR <70536705+LuizMiguelSR@users.noreply.github.com>
2020-10-13 21:06:20 -0300

    Add files via upload

---
+++ PHP/Aulas/ola_mundo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá_Mundo</title>
+    <style>
+        h2 {
+            color:aqua
+            text-shadow 1px 1px 1px black;
+        }
+    </style>
+</head>
+<body>
+    <h1>Testando PHP</h1>
+    <?php
+        echo "<h2>Olá Mundo</h2>";
+    ?>
+</body>
+</html>

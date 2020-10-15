407a967c385b20dfbba2549c3263648796247077
LuizMiguelSR <70536705+LuizMiguelSR@users.noreply.github.com>
2020-10-14 21:00:27 -0300

    Add files via upload

---
+++ PHP/Aulas/Aula04/tipos.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-be">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="_css/estilo.css"/>
+    <title>Tipos de Variáveis</title>
+</head>
+<body>
+<div>
+    <?php
+        $n=4;
+        $no="Luiz";
+        $idade=30;
+        echo "$no tem $idade anos.";
+    ?>
+</div>
+</body>
+</html>
